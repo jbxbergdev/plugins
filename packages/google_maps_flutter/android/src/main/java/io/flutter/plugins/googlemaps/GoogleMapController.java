4d05f6ae02c74ae5e38d06187b8924ebb2a0cb09
Johannes Bolz <bolz@365farmnet.com>
2019-12-21 12:10:58 +0100

    added comment

--- packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
+++ packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
@@ -511,6 +511,8 @@ final class GoogleMapController
       return;
     }
     mapView.onResume();
+    // Workaround for https://github.com/flutter/flutter/issues/40284
+    // This apparently forces a re-render of the map.
     if (googleMap != null) {
       googleMap.setMapType(googleMap.getMapType());
     }
